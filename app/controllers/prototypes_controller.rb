ae15dd4293b3a96d33c2aa185c410505ae72e747
bakuchunin <bakuchunin@gmail.com>
2021-09-02 16:54:47 +0900

    second

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,7 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_prototype, except: [:index, :new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
 
   def index
     @prototypes = Prototype.includes(:user)
@@ -13,10 +14,6 @@ class PrototypesController < ApplicationController
   end
   
   def edit
-    @prototype = Prototype.find(params[:id])
-    unless user_signed_in?
-      redirect_to action: :edit
-    end
   end
 
   def update
@@ -54,5 +51,13 @@ class PrototypesController < ApplicationController
   
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @prototype.user
   end
 end
